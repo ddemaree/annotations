636dcac4a2e3973106fd31557005076e587fc9b1
David Demaree <ddemaree@adobe.com>
2012-05-02 18:50:22 -0500

    Fixed issue where tags weren't actually being set in RakeTask

--- lib/annotations/rake_task.rb
+++ lib/annotations/rake_task.rb
@@ -10,6 +10,7 @@ module Annotations
     def initialize(name = :notes)
       @name = name
       @tags = [:optimize, :fixme, :todo]
+      yield self
       define
     end
 
@@ -38,32 +39,6 @@ module Annotations
           SourceAnnotationExtractor.enumerate ENV['ANNOTATION']
         end
       end
-
-      # desc name == :notes ? "Compile assets" : "Compile #{name} assets"
-      # desc "Enumerate all annotations (use notes:optimize, :fixme, :todo for focus)"
-      # task name do
-      #   with_logger do
-      #     manifest.compile(assets)
-      #   end
-      # end
-
-      # desc name == :assets ? "Remove all assets" : "Remove all #{name} assets"
-      # task "clobber_#{name}" do
-      #   with_logger do
-      #     manifest.clobber
-      #   end
-      # end
-
-      # task :clobber => ["clobber_#{name}"]
-
-      # desc name == :assets ? "Clean old assets" : "Clean old #{name} assets"
-      # task "clean_#{name}" do
-      #   with_logger do
-      #     manifest.clean(keep)
-      #   end
-      # end
-
-      # task :clean => ["clean_#{name}"]
     end
 
   end
